d56dcd61d44e78f34412bb3814dbce274a84033c
unknown <emadbebo729@gmail.com>
2022-02-11 17:43:52 +0200

    create modern ui ux project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import { Blog , Header , Possibility , Footer , Features , WhatGPT3} from './containers'
+import {Brand , Cta , Navbar } from './components'
+import './App.css'
+const App = () => {
+    return (
+        <div className='App '>
+            <div className='gradiant__bg'>
+                <Navbar />
+                <Header />
+            </div> 
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+             <Cta />
+             <Footer />
+        </div>
+    )
 }
 
-export default App;
+export default App
